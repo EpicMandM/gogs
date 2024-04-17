de19006331eb3cfe9475244f8e709310aaa29136
Vladyslav <epicmand.mi@gmail.com>
2024-04-17 14:38:04 +0300

    Added ssh key resolving for ansible

--- configure-named-profile.sh
+++ configure-named-profile.sh
@@ -10,3 +10,11 @@ aws configure set region $AWS_REGION --profile $PROFILE_NAME
 
 # Verify that profile is configured
 aws configure list --profile $PROFILE_NAME
+
+echo "Retrieving SSH key from Secrets Manager"
+
+SSH_KEY=$(aws secretsmanager get-secret-value --secret-id app-key-pair | jq -r '.SecretString')
+
+echo "$SSH_KEY" > ~/.ssh/id_rsa
+
+chmod 600 ~/.ssh/id_rsa
