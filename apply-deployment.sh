4e76a92d7dc795df51027769044b55ed5f2cc5c4
V <epicmand.mi@gmail.com>
2024-04-18 12:26:47 +0000

    Edited apply-deployment.sh

--- apply-deployment.sh
+++ apply-deployment.sh
@@ -7,9 +7,9 @@ terraform init -backend-config="access_key=$AWS_ACCESS_KEY" -backend-config="sec
 
 terraform destroy -auto-approve
 
-terraform apply -auto-approve -target=aws_ec2_transit_gateway.example_tgw 
-terraform apply -auto-approve -target=aws_vpc.gogs_vpc
-terraform apply -auto-approve -target=aws_subnet.gogs_public_subnet
-terraform apply -auto-approve -target=aws_subnet.gogs_private_subnet
+#terraform apply -auto-approve -target=aws_ec2_transit_gateway.example_tgw 
+#terraform apply -auto-approve -target=aws_vpc.gogs_vpc
+#terraform apply -auto-approve -target=aws_subnet.gogs_public_subnet
+#terraform apply -auto-approve -target=aws_subnet.gogs_private_subnet
 
-terraform apply -auto-approve
+#terraform apply -auto-approve
