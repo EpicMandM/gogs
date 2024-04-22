ba4d86201e98bcf6dccdb88a7677fdc1279eaf52
Vladyslav <epicmand.mi@gmail.com>
2024-04-22 21:30:59 +0300

    hotfix

--- install-ansible.sh
+++ install-ansible.sh
@@ -8,6 +8,21 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+yum install -y python3 python3-pip
+
+echo "Installing Ansible and dependencies..."
+pip3 install ansible boto3 botocore
+
+echo "Verifying Ansible installation..."
+ansible --version
+if [ $? -ne 0 ]; then
+    echo "Ansible installation failed."
+    exit 1
+fi
+
+
+echo "Installing git..."
+yum install -y git
 
 echo "Cloning repository and configuring SSH key..."
 git clone https://github.com/EpicMandM/gogs.git && cd gogs
