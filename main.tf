5eb87e6bc65a3e616c44c4fb79388a9481ca131a
Vladyslav <epicmand.mi@gmail.com>
2024-04-22 17:51:32 +0300

    added psql port

--- main.tf
+++ main.tf
@@ -247,6 +247,13 @@ resource "aws_security_group" "ec2_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
