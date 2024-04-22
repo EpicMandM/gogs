adc5436dde7ffb9c739e1b6efd1124fa8ade8405
Vladyslav <epicmand.mi@gmail.com>
2024-04-22 17:19:25 +0300

    test

--- apply-deployment.sh
+++ apply-deployment.sh
@@ -5,6 +5,6 @@ set -eu
 
 terraform init -backend-config="access_key=$AWS_ACCESS_KEY" -backend-config="secret_key=$AWS_SECRET_ACCESS_KEY"
 
-#terraform destroy -auto-approve
+terraform destroy -auto-approve
 
 terraform apply -auto-approve
